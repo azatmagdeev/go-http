d10429cf8d4881ebbe64ddb836c04b644168ef9d
PetrovaLiina <linamrrmrr@gmail.com>
2022-05-21 13:03:46 +0300

    chore:initial

---
+++ cmd/server.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		body := []byte("GO ok!") //string
+		writer.WriteHeader(200)
+		writer.Write(body)
+
+	})
+	err := http.ListenAndServe("localhost:9999", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
